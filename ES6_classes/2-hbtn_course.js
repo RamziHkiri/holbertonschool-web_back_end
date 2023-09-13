50fd6e1cad48cc5a0b01589ae1f9ef8b50fc1ac3
RamziHkiri <6257@holbertonstudents.com>
2023-09-13 13:05:12 +0200

    2-hbtn_course.js

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,32 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    this._students = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    this._students = value;
+  }
+}
+  
