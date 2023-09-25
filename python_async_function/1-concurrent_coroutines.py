932e23653ff71c8ac807db71a1cc3df48536b656
RamziHkiri <6257@holbertonstudents.com>
2023-09-25 16:48:28 +0200

    1-concurrent_coroutines.py

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Python - Async task 1 """
+
+import asyncio
+import random
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """  return the list of all the delays in ascending order """
+    delays = []
+    for i in range(n):
+        delays.append(asyncio.create_task(wait_random(max_delay)))
+    return [await delay for delay in asyncio.as_completed(delays)]
