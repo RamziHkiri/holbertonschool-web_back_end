70dbb30fd7204aa162f6738c44eaa3c918d4d6aa
RamziHkiri <6257@holbertonstudents.com>
2023-10-02 17:29:49 +0200

    2-then.js

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,10 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-        reject(new Error('The fake API is not working currently'));
-    }
-    console.log("Got a response from the API");
-    });
+  return promise.then(() => ({status: 200, body: "success" }))
+  .catch(() => Error())
+  .finally(() => { console.log("Got a response from the API")});
 }
