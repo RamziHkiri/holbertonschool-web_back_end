1dd83ea1ff100abe9bc751af9cf69c46679bffcb
RamziHkiri <6257@holbertonstudents.com>
2023-10-06 13:22:51 +0200

    6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,7 +3,7 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName , lastName, fileName) {
   const signingUser = await signUpUser(firstName, lastName).then((data) => ({
-    status: 'settled',
+    status: 'fulfilled',
     value: data,
   }));
     
