0956a5920860b2beaaa29b98a500ad86a5514dbb
RamziHkiri <6257@holbertonstudents.com>
2023-10-10 12:06:20 +0200

    9-groceries_list.js

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const grocerieslist = new Map();
+  grocerieslist.set('Apples', 10);
+  grocerieslist.set('Tomatoes', 10);
+  grocerieslist.set('Pasta', 1);
+  grocerieslist.set('Rice', 1);
+  grocerieslist.set('Banana', 5);
+  return grocerieslist;
+}
