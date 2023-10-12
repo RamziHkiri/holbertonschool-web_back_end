9103870045d52060a0c8f27200d5c82a14f4c60e
RamziHkiri <6257@holbertonstudents.com>
2023-10-12 19:43:10 +0200

    10-update_topics.py

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-'''
-Change the topics of a school document based on the name
-'''
+"""Change the topics of a school document based on the name"""
 
 
 def update_topics(mongo_collection, name, topics):
