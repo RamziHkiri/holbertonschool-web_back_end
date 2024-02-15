addae452783d25ae344a66fa9b20f09ad465da87
RamziHkiri <6257@holbertonstudents.com>
2024-02-15 19:11:38 +0100

    0-basic_cache.py fix PEP8 style

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-Create a class BasicCache that inherits from BaseCaching and is a caching system
+Create class BasicCache that inherits from BaseCaching and is a caching system
 """
 BaseCaching = __import__('base_caching').BaseCaching
+
 
 class BasicCache(BaseCaching):
     """
@@ -10,13 +11,14 @@ class BasicCache(BaseCaching):
     """
     def put(self, key, item):
         """
-        assign to the dictionary self.cache_data the item value for the key key.
+        assign to the dictionary self.cache_data
+        the item value for the key key.
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
-    
+
     def get(self, key):
-        """ 
+        """
          return the value in self.cache_data linked to key
         """
         if key is not None and key in self.cache_data:
