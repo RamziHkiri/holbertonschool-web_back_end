cc65174fb783856d446c31d76df33187ab217c7d
RamziHkiri <6257@holbertonstudents.com>
2024-02-17 11:16:59 +0100

    test

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Create a class LRUCache that inherits from BaseCaching and is a caching system
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    class LRUCache that inherits from BaseCaching
+    """
+
+    def __init__(self):
+        """initilize instance"""
+        super().__init__()
+        self.preorities ={}
+        self.preority = 0
+        self.minKey=''
+
+    def put(self, key, item):
+        """you must discard the least recently used item (LRU algorithm)"""
+        if key is not None and item is not None:
+            self.preority += 1
+            self.cache_data[key] = item
+            self.preorities[key] = self.preority
+            
+            if len(self.preorities) > BaseCaching.MAX_ITEMS:
+                self.minKey = min(self.preorities, key=lambda k: self.preorities[k])
+                del self.cache_data[self.minKey]
+                del self.preorities[self.minKey]
+                print('DISCARD: {:s}'.format(self.minKey))
+                
+        
+        
+        
+    def get(self, key):
+        """
+         return the value in self.cache_data linked to key
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None   
