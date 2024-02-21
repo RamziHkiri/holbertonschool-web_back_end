ae42f220fb987a91072ac8d1c6f1cb60ae9604ea
RamziHkiri <6257@holbertonstudents.com>
2024-02-21 17:40:36 +0100

    filtered_logger.py

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -40,6 +40,6 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """"""
+        """ filter values in incoming log records using filter_datum"""
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
