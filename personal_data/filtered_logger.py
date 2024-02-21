5fb36b1f9aa95e907106b2ee4ca58e4bf517b2b6
RamziHkiri <6257@holbertonstudents.com>
2024-02-21 15:42:49 +0100

    filtered_logger.py

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,9 +3,10 @@
 the log message obfuscated.
 """
 import re
+from typing import List
 
 
-def filter_datum(fields: list[str], redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
     function called filter_datum that returns the log message obfuscated
