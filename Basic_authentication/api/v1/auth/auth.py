9bf8e36ac6ceb92019fd64d03a869c7429ce8097
RamziHkiri <6257@holbertonstudents.com>
2024-02-23 19:39:12 +0100

    Basic_authentication/

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""create the class auth"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth():
+    """class auth"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require auth"""
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """authorization_header""" 
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current_user"""
+        return None
