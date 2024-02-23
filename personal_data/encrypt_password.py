a4e4e8cc36af32194954ca6fc937f516588ef880
RamziHkiri <6257@holbertonstudents.com>
2024-02-23 15:51:42 +0100

    encrypt_password.py

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -11,3 +11,9 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password_bytes, salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ is_valid function that expects 2 arguments and returns a boolean."""
+    password_bytes = password.encode('utf-8')
+    return bcrypt.checkpw(password_bytes, hashed_password)
