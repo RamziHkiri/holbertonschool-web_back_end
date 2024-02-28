92bdb7a8dba0780b6e97123a1481143abb7ac1a8
RamziHkiri <6257@holbertonstudents.com>
2024-02-28 18:18:11 +0100

    Basic_authentication/

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """class BasicAuth enherit from Auth"""
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -16,3 +17,18 @@ class BasicAuth(Auth):
             return None
         second_part = authorization_header.split(' ', 1)[1]
         return second_part
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """decode_base_64"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_byte = b64decode(base64_authorization_header)
+            decoded_string = decoded_byte.decode('utf-8')
+            return decoded_string
+        except BaseException:
+            return None
