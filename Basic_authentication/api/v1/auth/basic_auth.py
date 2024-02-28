1a16ed61b5ed3d73e13b82b141c6a18712a0ec89
RamziHkiri <6257@holbertonstudents.com>
2024-02-28 18:34:26 +0100

    B

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -32,3 +32,17 @@ class BasicAuth(Auth):
             return decoded_string
         except BaseException:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """extract_user_credentials"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        user = decoded_base64_authorization_header.split(':', 1)[0]
+        password = decoded_base64_authorization_header.split(':', 1)[1]
+        return user, password
