54d09b5f45967ed8937b3019c3417cdba071d5f1
RamziHkiri <6257@holbertonstudents.com>
2024-02-28 14:05:02 +0100

    Basic_authentication/

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -12,9 +12,8 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
 AUTH_TYPE = os.getenv("AUTH_TYPE")
-if auth:
+if AUTH_TYPE:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -22,6 +22,7 @@ class Auth():
         """authorization_header"""
         if request is None:
             return None
+
         return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar('User'):
