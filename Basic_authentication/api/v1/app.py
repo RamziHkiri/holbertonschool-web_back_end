2e84c7aba9dcdf83534d2bec84e7ed706585fef4
RamziHkiri <6257@holbertonstudents.com>
2024-02-28 13:21:55 +0100

    Basic_authentication/

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -13,31 +13,10 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+AUTH_TYPE = os.getenv("AUTH_TYPE")
 if auth:
     from api.v1.auth.auth import Auth
     auth = Auth()
-
-
-@app.before_request
-def before_request() -> str:
-    """ Before Request Handler
-    Requests Validation
-    """
-    if auth is None:
-        return
-
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-
-    if auth.authorization_header(request) is None:
-        abort(401)
-
-    if auth.current_user(request) is None:
-        abort(403)
 
 
 @app.errorhandler(404)
@@ -59,6 +38,29 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.before_request
+def before_request() -> str:
+    """ Before Request Handler
+
+    Requests Validation
+    """
+    if auth is None:
+        return
+
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
