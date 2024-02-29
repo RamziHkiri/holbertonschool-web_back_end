62dfe325214c77fbe41c28ca2e125f6f024369e2
RamziHkiri <6257@holbertonstudents.com>
2024-02-29 15:24:31 +0100

    Session_authentication/

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -49,7 +49,6 @@ def not_found(error) -> str:
 @app.before_request
 def before_request() -> str:
     """ Before Request Handler
-
     Requests Validation
     """
     if auth is None:
@@ -57,7 +56,8 @@ def before_request() -> str:
 
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
 
     if not auth.require_auth(request.path, excluded_paths):
         return
@@ -67,6 +67,10 @@ def before_request() -> str:
 
     if auth.current_user(request) is None:
         abort(403)
+    if auth.authorization_header(request) is None\
+       and auth.session_cookie(request) is None:
+        abort(401)
+
     request.current_user = auth.current_user(request)
 
 
