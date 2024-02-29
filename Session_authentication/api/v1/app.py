eb517fd05ccecbd04d690b989ec420fac7963f8b
RamziHkiri <6257@holbertonstudents.com>
2024-02-29 15:30:57 +0100

    Session_authentication/

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -71,7 +71,11 @@ def before_request() -> str:
        and auth.session_cookie(request) is None:
         abort(401)
 
-    request.current_user = auth.current_user(request)
+    current_user = auth.current_user(request)
+    if current_user is None:
+        abort(403)
+
+    request.current_user = current_user
 
 
 if __name__ == "__main__":
