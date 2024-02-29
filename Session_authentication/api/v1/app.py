5f7a7859eec91d6c8d42c99101a4a8f327f67549
RamziHkiri <6257@holbertonstudents.com>
2024-02-29 13:56:07 +0100

    Session_authentication/

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -17,9 +17,12 @@ AUTH_TYPE = os.getenv("AUTH_TYPE")
 if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif AUTH_TYPE == "basic_auth":
+if AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+if AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import Session_auth
+    auth = Session_auth()
 
 
 @app.errorhandler(404)
