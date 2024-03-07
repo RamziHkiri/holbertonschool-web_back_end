4f3df0c17e92772e384c307495519d70a78c6286
RamziHkiri <6257@holbertonstudents.com>
2024-03-07 13:18:11 +0100

    Session_authentication/

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ view that handles all routes for the Session authentication."""
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models.user import User
 from os import getenv
@@ -42,3 +42,20 @@ def login():
     response.set_cookie(SESSION_NAME, session_id)
 
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def logout():
+    """ DELETE /auth_session/logout
+    Return:
+        - Empty dictionary if succesful
+    """
+    from api.v1.app import auth
+
+    deleted = auth.destroy_session(request)
+
+    if not deleted:
+        abort(404)
+
+    return jsonify({}), 200
