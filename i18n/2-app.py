77dfa69fed5fc95a35f14833920590990b4313a7
RamziHkiri <6257@holbertonstudents.com>
2024-03-20 03:03:33 +0100

    2-app.py

---
+++ i18n/2-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" Route module for the API - Basic Flask app """
+
+from flask import Flask, render_template, request
+from os import getenv
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ configure available languages in our app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """return or render the index.htmel"""
+    return render_template('0-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
