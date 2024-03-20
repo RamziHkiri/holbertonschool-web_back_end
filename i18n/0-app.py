377a63bda4ac4e51de12e1c3f36a61a19265e310
RamziHkiri <6257@holbertonstudents.com>
2024-03-20 02:48:58 +0100

    0-app.py

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
+from os import getenv
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    """return or render the index.htmel"""
     return render_template('0-index.html')
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
