486b047c47ccd4ce6bd53167d7752f0d515186de
RamziHkiri <6257@holbertonstudents.com>
2024-03-20 03:00:30 +0100

    1-app.py

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -6,16 +6,18 @@ from os import getenv
 from flask_babel import Babel
 
 app = Flask(__name__)
-
 babel = Babel(app)
 
 
 class Config:
+    """ configure available languages in our app"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
+
 
 @app.route('/')
 def index():
