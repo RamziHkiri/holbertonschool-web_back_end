0d05260606e8c670c0e61a4182f5edf07655287a
RamziHkiri <6257@holbertonstudents.com>
2024-03-23 16:18:05 +0100

    test

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -26,6 +26,25 @@ def call_history(method: Callable) -> Callable:
     return wrapper
 
 
+def replay(method: Callable):
+    """ replay to display hstory of calls"""
+    self_ = method.__self__
+    stored_name = method.__qualname__
+    stored_key = self_.get(stored_name)
+    if stored_key:
+        times = self_.get_str(stored_key)
+        inputs = self_._redis.lrange(stored_name + ":inputs", 0, -1)
+        outputs = self_._redis.lrange(stored_name + ":outputs", 0, -1)
+
+        print(f"{stored_name} was called {times} times:")
+        zipvalues = zip(inputs, outputs)
+        result_list = list(zipvalues)
+        for k, v in result_list:
+            name = self_.get_str(k)
+            val = self_.get_str(v)
+            print(f"{stored_name}(*{name}) -> {val}")
+
+
 def count_calls(method: Callable) -> Callable:
     """ Decortator for counting how many times a function
     has been called """
