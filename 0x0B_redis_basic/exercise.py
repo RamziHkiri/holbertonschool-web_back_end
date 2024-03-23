640212b2e5245cead7b204eb81b71169b81ee3a3
RamziHkiri <6257@holbertonstudents.com>
2024-03-23 15:13:00 +0100

    create the get methode with get str and getint

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """implement the class Cache"""
 
-from typing import Union
+from typing import Callable, Optional, Union
 from uuid import uuid4
 import redis
 
@@ -22,3 +22,12 @@ class Cache:
         self._redis.set(random_key, data)
 
         return random_key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ Reading from Redis and recovering original type """
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+
+        return value
