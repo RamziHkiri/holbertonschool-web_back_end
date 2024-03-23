96ff77e1a77ed092cb31ca1be204f4b963b2ea83
RamziHkiri <6257@holbertonstudents.com>
2024-03-23 16:01:39 +0100

    create the count_calls method

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python3
 """implement the class Cache"""
 
+import functools
 from typing import Callable, Optional, Union
 from uuid import uuid4
 import redis
+
+
+def count_calls(method: Callable) -> Callable:
+    """ Decortator for counting how many times a function
+    has been called """
+
+    key = method.__qualname__
+
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper for decorator functionality """
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache:
@@ -14,6 +30,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store the input data in Redis using a
         random key and return the key.
