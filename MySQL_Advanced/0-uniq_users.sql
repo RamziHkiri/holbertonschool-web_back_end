23d3af2c24abe9cb424fdd1906f807ffdf896b1b
RamziHkiri <6257@holbertonstudents.com>
2024-03-24 13:51:18 +0100

    fix

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -3,8 +3,8 @@
 --  Your script can be executed on any database
 ----------------------------------------------------------------
 
-CREATE TABLE If NOT EXISTS 'users' (
-    'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    'email'varchar(255) NOT NULL UNIQUE,
-    'name' varchar(255) NOT NULL
-)
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
