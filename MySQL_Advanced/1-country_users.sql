bc469438572b44822a3a45d43c820e1a0c7a7a6f
RamziHkiri <6257@holbertonstudents.com>
2024-03-24 14:39:49 +0100

    1-country_users.sql

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -2,8 +2,8 @@
 --  If the table already exists, your script should not fail
 
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL,
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL
 );
