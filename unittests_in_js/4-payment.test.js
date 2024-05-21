404c3324bb70cc4b362c92075950541790f7e12b
RamziHkiri <6257@holbertonstudents.com>
2024-05-21 15:41:11 +0200

    4-payment.

---
+++ unittests_in_js/4-payment.test.js
@@ -0,0 +1,16 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', function () {
+  it('should stub Utils.calculateNumber to return 10 and verify console.log output', function () {
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const consoleLogSpy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
+  });
+});
