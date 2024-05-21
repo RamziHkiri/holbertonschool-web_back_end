80c3bc6396e45cd6d60adfd963b42e9945b8dd94
RamziHkiri <6257@holbertonstudents.com>
2024-05-21 16:30:46 +0200

    6-payment_token.js

--- unittests_in_js/6-payment_token.test.js
+++ unittests_in_js/6-payment_token.test.js
@@ -1,16 +1,18 @@
+const mocha = require('mocha');
 const { expect } = require('chai');
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
+const sinon = require('sinon');
+
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
-chai.use(chaiAsPromised);
-
-describe('getPaymentTokenFromAPI', function () {
-  it('should return a resolved promise with the correct object when success is true', function () {
-    return expect(getPaymentTokenFromAPI(true)).to.eventually.deep.equal({ data: 'Successful response from the API' });
-  });
-
-  it('should do nothing when success is false', function () {
-    return expect(getPaymentTokenFromAPI(false)).to.be.undefined;
+describe('getPaymentTokenFromAPI', () => {
+  it('should resolve promise if success is true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        expect(res).to.eql({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
   });
 });
