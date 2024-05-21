2c1522d450a1ffd625499c5c3d076ce9e1e3800c
RamziHkiri <6257@holbertonstudents.com>
2024-05-21 15:14:10 +0200

    3-payment.

---
+++ unittests_in_js/3-payment.test.js
@@ -0,0 +1,16 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', function () {
+  it('should call Utils.calculateNumber with type SUM and the correct arguments', function () {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+
+    calculateNumberSpy.restore();
+  });
+});
